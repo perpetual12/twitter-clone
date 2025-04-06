62d220079df3bc812af1e6538f052732edcb20bb
perpetual <obasekiperpetual@gmail.com>
2025-04-06 09:20:40 +0100

    Update made

--- backend/server.js
+++ backend/server.js
@@ -37,12 +37,13 @@ app.use("/api/posts", postRoutes);
 app.use("/api/notifications", notificationRoutes);
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-    });
+    
 }
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+});
 
 
 
